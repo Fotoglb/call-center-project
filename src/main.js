1520e41ec7c84db8c3449411a63c01f23f99f744
Eman Swerky <eman.mahmoud.swerky@gmail.com>
2026-07-25 17:43:57 -0700

    add font

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import '@fontsource/tajawal/400.css'
+import '@fontsource/tajawal/500.css'
+import '@fontsource/tajawal/700.css'
+import '@fontsource/tajawal/800.css'
+import '@fontsource/tajawal/900.css'
 import '@/styles/main.css'
 
 const app = createApp(App)
